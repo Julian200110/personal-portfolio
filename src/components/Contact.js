03f85d7189275249306a2b00d7a5b46f6182e68d
Julian <vanegas2001@gmail.com>
2025-01-27 18:54:08 -0500

    Second commit

---
+++ src/components/Contact.js
@@ -0,0 +1,159 @@
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/img/julian2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import emailjs from "@emailjs/browser";
+
+export const Contact = () => {
+  const formInitialDetails = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState("Enviar");
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+    setFormDetails({
+      ...formDetails,
+      [category]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Enviando...");
+
+    // Configuración de EmailJS
+    const serviceID = "service_7a55ghb";
+    const templateID = "template_6oktb1u";
+    const publicKey = "A8WbuwBuAv7mC5sj1";
+
+    try {
+      const response = await emailjs.send(
+        serviceID,
+        templateID,
+        formDetails,
+        publicKey
+      );
+      setButtonText("Enviar");
+      setFormDetails(formInitialDetails);
+      if (response.status === 200) {
+        setStatus({ success: true, message: "Correo enviado exitosamente" });
+      }
+    } catch (error) {
+      console.error("Error al enviar el correo:", error);
+      setStatus({
+        success: false,
+        message: "Algo salió mal. Intenta de nuevo.",
+      });
+    }
+  };
+
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <img
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                  src={contactImg}
+                  alt="Contact Us"
+                />
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Deja tu información</h2>
+                  <form onSubmit={handleSubmit}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          value={formDetails.firstName}
+                          placeholder="Primer Nombre"
+                          onChange={(e) =>
+                            onFormUpdate("firstName", e.target.value)
+                          }
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          value={formDetails.lastName}
+                          placeholder="Segundo Nombre"
+                          onChange={(e) =>
+                            onFormUpdate("lastName", e.target.value)
+                          }
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="email"
+                          value={formDetails.email}
+                          placeholder="Correo electronico"
+                          onChange={(e) =>
+                            onFormUpdate("email", e.target.value)
+                          }
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="tel"
+                          value={formDetails.phone}
+                          placeholder="Numero telefonico"
+                          onChange={(e) =>
+                            onFormUpdate("phone", e.target.value)
+                          }
+                        />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea
+                          rows="6"
+                          value={formDetails.message}
+                          placeholder="Deja tu mensaje"
+                          onChange={(e) =>
+                            onFormUpdate("message", e.target.value)
+                          }
+                        ></textarea>
+                        <button type="submit">
+                          <span>{buttonText}</span>
+                        </button>
+                      </Col>
+                      {status.message && (
+                        <Col>
+                          <p
+                            className={
+                              status.success === false ? "danger" : "success"
+                            }
+                          >
+                            {status.message}
+                          </p>
+                        </Col>
+                      )}
+                    </Row>
+                  </form>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Skills.js
@@ -0,0 +1,103 @@
+import html from "../assets/img/html.png";
+import css from "../assets/img/css.png";
+import javascript from "../assets/img/javascript.png";
+import react from "../assets/img/react.png";
+import Python from "../assets/img/python.png";
+import vue from "../assets/img/vue.png";
+import angular from "../assets/img/angularjs.png";
+import java from "../assets/img/java.png";
+import mongo from "../assets/img/mongo-db.png";
+import mysql from "../assets/img/mysql.png";
+import c from "../assets/img/c-language.png";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Habilidades</h2>
+              <p></p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="owl-carousel owl-theme skill-slider"
+              >
+                <div className="item">
+                  <img src={html} alt="Image" />
+                  <h5>HTML</h5>
+                </div>
+                <div className="item">
+                  <img src={css} alt="Image" />
+                  <h5>CSS</h5>
+                </div>
+                <div className="item">
+                  <img src={javascript} alt="Image" />
+                  <h5>JavaScript</h5>
+                </div>
+                <div className="item">
+                  <img src={vue} alt="Image" />
+                  <h5>Vue</h5>
+                </div>
+                <div className="item">
+                  <img src={angular} alt="Image" />
+                  <h5>Angular</h5>
+                </div>
+                <div className="item">
+                  <img src={react} alt="Image" />
+                  <h5>React</h5>
+                </div>
+                <div className="item">
+                  <img src={Python} alt="Image" />
+                  <h5>Python</h5>
+                </div>
+                <div className="item">
+                  <img src={java} alt="Image" />
+                  <h5>Java</h5>
+                </div>
+                <div className="item">
+                  <img src={mongo} alt="Image" />
+                  <h5>mongoDB</h5>
+                </div>
+                <div className="item">
+                  <img src={mysql} alt="Image" />
+                  <h5>Mysql</h5>
+                </div>
+                <div className="item">
+                  <img src={c} alt="Image" />
+                  <h5>c++</h5>
+                </div>
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
